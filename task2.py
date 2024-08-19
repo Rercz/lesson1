13f83e6b7eec45f77b6573be00bbc6336eab5a0c
Rercz <serlook@yandex.ru>
2024-08-20 09:15:48 +1000

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,6 @@
+print(9 ** 0.5 * 5) # 1st program
+print(99.9 > 99.8, 1000 != 1000.1) # 2nd program
+print(99.9 > 99.8 and 1000 != 1000.1)
+print(99.9 > 99.8 or 1000 != 1000.1)
+print(2 * 2 + 2 == 2 * (2 + 2))  # 3rd program
+
