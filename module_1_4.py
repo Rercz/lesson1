3118432bd28482252fb652e9cb37e502a5946444
Rercz <serlook@yandex.ru>
2024-08-20 19:30:48 +1000

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = 'Организация программ и методы строк'
+print(my_string)
+print(my_string. upper())
+print(my_string. lower())
+print(my_string[0])
+print(my_string[-1])
+print(my_string.replace(' ', ''))
