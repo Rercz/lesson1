66985d6dfd376db9f4d2528bfe2cc8461e11af31
Rercz <serlook@yandex.ru>
2024-08-23 13:25:36 +1000

    Add files via upload

--- module_1_5.py
+++ module_1_5.py
@@ -1,8 +1,7 @@
-immutable_var = [1, 2, 3, True]
-tuple = immutable_var
-print(tuple)
-#immutable_var = [1, 2, 3, false]   #неизменяемость вляется особенностью кортежа (tupl),поэтому: NameError: name 'false' is not defined. Did you mean: 'False'?
-mutable_list = [1, 2, 3, 'Как уже достал этот дождь']
+immutable_var = 1, 2, 3, 'это кошка'
+print(immutable_var)
+#immutable_var[3] = 4 TypeError: 'tuple' object does not support item assignment потому что картеж
+mutable_list = [1, 2, 3, 'это кот']
 print(mutable_list)
-mutable_list[3] = 'Как я любдю дождь'
+mutable_list[3] = 3
 print(mutable_list)
