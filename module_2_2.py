49e507680c9178d017681cad4a6847bdbee32fac
Rercz <serlook@yandex.ru>
2024-08-28 20:56:18 +1000

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,15 @@
+first = int(input('Ведите число'))
+second  = int(input('Ведите число'))
+third   = int(input('Ведите число'))
+if first == second == third:
+    print('3')
+elif first != second and third:
+ if second != third and first:
+    if third != first and second:
+     print('0')
+if first == second and first != third and second != third:
+     print('2')
+if first == third and third != second and first != second:
+     print('2')
+if second == third and first != second and first != third:
+     print('2')
